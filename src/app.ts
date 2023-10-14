06f3cb458b5d61fc8062ee6166b91765b9de8933
Kevin Marte <kevinprogramadortecnico@gmail.com>
2023-10-14 08:01:37 -0400

    Comando de información de usuario

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { Client, CommandInteraction, GatewayIntentBits } from "discord.js";
 import EnviarEmbed_Bienvenida from "./Bienvenida/enviarEmbed";
 import EnviarEmbed_Despedida from "./Despedida/enviarEmbed";
+import info_usuario from "./Comandos/Utilidad/usuario";
+
 
 const client = new Client({
   intents: [
@@ -23,13 +25,28 @@ client.on("ready", () => {
 
 
 //Codigo a ejecutar cuando un usuario se sale de uno de los tres servidores
+//para mandar el mensaje de despedida en el canal correspondiente
 client.on("guildMemberRemove", member=>{
   EnviarEmbed_Despedida(member, member.guild.id)
 })
 
 
 //Codigo que se ejecutara cuando entre un miembro a uno de los tres servidores
-//
+//para mandar el mensaje de bienvenida en el canal correspondiente
 client.on("guildMemberAdd", member=>{
   EnviarEmbed_Bienvenida(member, member.guild.id)
+})
+
+//Codigo que se ejecuta al momento de que un usuario usa un comando
+client.on("interactionCreate", interaction=>{
+  if (!interaction.isChatInputCommand()) return;
+
+  switch (interaction.commandName){
+    case "usuario":
+      info_usuario(interaction)
+      break;
+
+    default:
+      break;
+  }
 })
